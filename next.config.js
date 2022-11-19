26ce544848f4f9aa8f95f2ca80b05ddcb1798f58
Yash Sonalia <sonaliayash@gmail.com>
2022-11-19 13:02:21 +0530

    Project setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	compiler: {
+		styledComponents: {
+			ssr: true,
+			displayName: true,
+		},
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
