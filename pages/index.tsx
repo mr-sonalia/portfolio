3d3091e172e3579261176e72c5f3e1082b276346
Yash Sonalia <sonaliayash@gmail.com>
2022-11-26 15:34:48 +0530

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import { Navbar, ThemeToggle } from "components";
-import { useTheme } from "../hooks";
-import { Main } from "../layouts";
+import { useTheme } from "hooks";
+import { Main } from "layouts";
 import { Experiences, Hero, Projects, Skills } from "sections";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "../styles";
+import { GlobalStyle } from "styles";
 
 export default function Home() {
 	const [theme, mode, setTheme, _getTheme] = useTheme();
