e8476a1bf97aec625665578993d25b5c4c4cec10
Yash Sonalia <53109900+mr-sonalia@users.noreply.github.com>
2025-01-05 09:12:27 +0000

    (update) work section + minor refactoring

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,8 +1,10 @@
 const Footer = () => {
+	const currentYear = (new Date()).getFullYear();
+
 	return (
 		<section id="contact" className="mt-20 md:mt-40 container w-full">
 			<p className="text-center text-sm mb-1 text-gray-800 dark:text-gray-300 leading-6">
-				©2022-2024. Made with ❤️, by Yash Sonalia.
+				©2022-{currentYear}. Made with ❤️, by Yash Sonalia.
 			</p>
 			<p className="text-center text-sm mb-3 text-gray-800 dark:text-gray-300 leading-6">
 				🤙🏼 +91 704-402-0012 <span className="text-sm text-accent-500 select-none"> • </span>{" "}
